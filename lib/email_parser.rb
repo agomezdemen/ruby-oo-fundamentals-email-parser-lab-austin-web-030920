e049a7251f8eaae6b7ba48d2608c70f726ba4d40
munificent-cast-9719 <agomezdemen15@gmail.com>
2020-03-10 18:36:25 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,9 +2,8 @@ class EmailAddressParser
   attr_accessor :emails
   
   def initialize(email_addresses)
-    array = Array.new()
-    array = email_addresses.split(",")
-    @emails = array
+    email_addresses.split(",")
+    @emails = email_addresses
   end
   
   
